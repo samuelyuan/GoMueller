7f01082bae866c3a6f837035640ae224e1b0bd2b
Samuel Yuan <yuans@rpi.edu>
2015-02-28 15:15:26 -0500

    Added delete functionality
    Users can delete their old exercise data

--- app/src/main/java/com/example/rcos/gomueller/ExerciseCRUD.java
+++ app/src/main/java/com/example/rcos/gomueller/ExerciseCRUD.java
@@ -24,9 +24,48 @@ public class ExerciseCRUD {
 
         long exercise_id = db.insert(Exercise.TABLE, null, values);
 
-
         db.close();
         return (int) exercise_id;
+    }
+
+    //Remove an entry from the database
+    //(NOTE: this function might remove two strings with the same weight and number of sets, like two strings with weight: 150, numSets: 8)
+    public void delete(String exerciseName, String currentDetailStr) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String selectQuery = "SELECT " + Exercise.keyWeight + " , " + Exercise.keyNumber
+                + " FROM " + Exercise.TABLE
+                + " WHERE " + Exercise.keyName + " = " + "\"" + exerciseName + "\"";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        String w8;
+        String numSet;
+        String detail;
+
+        //Go through each rows
+        if (cursor.moveToFirst()) {
+            do {
+                //form detail string
+                w8 = cursor.getString(cursor.getColumnIndex(Exercise.keyWeight));
+                numSet = cursor.getString(cursor.getColumnIndex(Exercise.keyNumber));
+                detail = "Weight: " + w8 + "      Set: " + numSet;
+
+                //delete this entry
+                if (detail.equals(currentDetailStr))
+                {
+                    db.delete(Exercise.TABLE, Exercise.keyWeight + "=" + w8 + " and " + Exercise.keyNumber + "=" + numSet, null);
+                }
+
+                //delete the exercise if needed
+                if (cursor.getCount() == 0) {
+                    db.delete(Exercise.TABLE, Exercise.keyName + " = " + "\"" + exerciseName + "\"", null);
+                    break;
+                }
+
+            } while (cursor.moveToNext());
+
+        }
+
+        cursor.close();
+        db.close();
     }
 
     public ArrayList<String> getExerciseArray() {
@@ -56,7 +95,7 @@ public class ExerciseCRUD {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         String selectQuery = "SELECT " + Exercise.keyWeight + " , " + Exercise.keyNumber
                 + " FROM " + Exercise.TABLE
-                +" WHERE " + Exercise.keyName + " = " + "\"" + item + "\"";
+                + " WHERE " + Exercise.keyName + " = " + "\"" + item + "\"";
         ArrayList<String> exerciseDetail = new ArrayList<String>();
         String w8;
         String numSet;
--- app/src/main/java/com/example/rcos/gomueller/ExerciseDetailActivity.java
+++ app/src/main/java/com/example/rcos/gomueller/ExerciseDetailActivity.java
@@ -8,7 +8,11 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.util.SparseBooleanArray;
 import java.util.ArrayList;
 
 
@@ -24,12 +28,39 @@ public class ExerciseDetailActivity extends ListActivity {
         setContentView(R.layout.activity_exercise_detail);
 
         Bundle bundle = getIntent().getExtras();
-        String message = bundle.getString("message");
+        final String message = bundle.getString("message");
 
-        ExerciseCRUD crudDetail = new ExerciseCRUD(this);
+        final ExerciseCRUD crudDetail = new ExerciseCRUD(this);
 
         detailArray = crudDetail.getExerciseDetail(message);
         adapter = new ArrayAdapter<String>(this, R.layout.row_layout, R.id.listText, detailArray);
+
+        Button btnDel = (Button) findViewById(R.id.btnDel);
+
+        //When delete button is pressed...
+        OnClickListener listenerDel = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get the checked items
+                SparseBooleanArray checkedItemPositions = getListView().getCheckedItemPositions();
+                int itemCount = getListView().getCount();
+
+                //Delete all the selected items
+                for(int i=itemCount-1; i >= 0; i--){
+                    if(checkedItemPositions.get(i))
+                    {
+                        crudDetail.delete(message, detailArray.get(i));
+                        adapter.remove(detailArray.get(i));
+                    }
+                }
+                checkedItemPositions.clear();
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        /** Setting the event listener for the delete button */
+        btnDel.setOnClickListener(listenerDel);
+
         setListAdapter(adapter);
     }
 
@@ -54,5 +85,16 @@ public class ExerciseDetailActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id) {
+        super.onListItemClick(list, view, position, id);
+
+        String selectedItem = (String) getListView().getItemAtPosition(position);
+
+        Toast.makeText(getApplicationContext(),
+                "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                .show();
     }
 }
