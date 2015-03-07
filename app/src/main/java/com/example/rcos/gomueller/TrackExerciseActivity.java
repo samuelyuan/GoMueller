c93216bbf04decefc51443794095cd94bc3679e5
Samuel Yuan <yuans@rpi.edu>
2015-03-07 15:46:11 -0500

    Changed the app's appearance
    -Added back button to each state
    -Allow user to change settings in any state

--- app/src/main/java/com/example/rcos/gomueller/TrackExerciseActivity.java
+++ app/src/main/java/com/example/rcos/gomueller/TrackExerciseActivity.java
@@ -39,6 +39,28 @@ public class TrackExerciseActivity extends ListActivity {
         setListAdapter(adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        getMenuInflater().inflate(R.menu.menu_track_exercise, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onListItemClick(ListView list, View view, int position, long id) {
